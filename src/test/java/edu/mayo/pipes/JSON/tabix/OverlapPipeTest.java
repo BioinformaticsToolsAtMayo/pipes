ba3ee1f4fb02892fe0b4fd8b1d78422bb27b63c7
m102417 <m102417@8f329c14-d232-4f17-8684-7ee34322b8dc>
2012-11-30 17:50:34 +0000

    non working overlap pipe

--- src/test/java/edu/mayo/pipes/JSON/tabix/OverlapPipeTest.java
+++ src/test/java/edu/mayo/pipes/JSON/tabix/OverlapPipeTest.java
@@ -35,44 +35,53 @@ public class OverlapPipeTest {
     public void tearDown() {
     }
 
+    
+    //Tests we need:
+    //1. if there are no matches in the query -- put empty thing in there and pass history along
+    //2. if there is only one match in the query, make sure history is passed along only once. do not add new line or new row
+    //3. if there are more than one match in the query, make sure that we have duplicated rows in the history for each match.
 
     public String dataFile = "src/test/resources/testData/tabix/example.gff.gz";
+    public String geneFile = "src/test/resources/testData/tabix/genes.tsv.bgz";
     public String tabixIndexFile = "src/test/resources/testData/tabix/example.gff.gz.tbi";
     
+    @Test
     public void testTQuery() throws IOException{
+        System.out.println("Test TQuery");
         String record = "";
+        String r1 = "abc123\t.\tgene\t6000\t12000\t.\t+\t.\tID=gene00005";
+        String r2 = "abc123\t.\tgene\t8000\t16000\t.\t+\t.\tID=gene00005";
         String query = "abc123:7000-13000";
         OverlapPipe op = new OverlapPipe(dataFile);
         Iterator records = op.tquery(query);
-        while((record = records.next()) != null){
-            System.out.println(record);
+        for(int i=1;(record = records.next()) != null; i++){
+            //System.out.println(record);
+            if(i==1){
+                assertEquals(r1,record);
+            }
+            if(i==2){
+                assertEquals(r2,record);
+            }
         }
     }
     
-    public void testQuery() throws IOException{
-        System.out.println("Test Query");
-        OverlapPipe op = new OverlapPipe(dataFile);
-    }
-    
-    
-    /**
-     * Test of query method, of class OverlapPipe.
-     */
     @Test
-    public void testQuery_String() throws IOException {
+    public void testQuery() throws IOException{
         System.out.println("Test Query Based on JSON input");
-        
-        String json = "";
-        OverlapPipe op = new OverlapPipe(dataFile);
-        String record;
-        Iterator records = op.query(json);        
-        while((record = records.next()) != null){
+        String record = "";
+        OverlapPipe op = new OverlapPipe(geneFile);
+        String brca1 = "{\"_type\":\"gene\",\"_landmark\":\"17\",\"_strand\":\"-\",\"_minBP\":41196312,\"_maxBP\":41277500,\"gene\":\"BRCA1\",\"gene_synonym\":\"BRCAI; BRCC1; BROVCA1; IRIS; PNCA4; PPP1R53; PSCP; RNF53\",\"note\":\"breast cancer 1, early onset; Derived by automated computational analysis using gene prediction method: BestRefseq.\",\"GeneID\":\"672\",\"HGNC\":\"1100\",\"HPRD\":\"00218\",\"MIM\":\"113705\"}";
+        Iterator records = op.query(brca1);
+        for(int i=1;(record = records.next()) != null; i++){
             System.out.println(record);
+            String[] s = record.split("\t");
+            assertEquals(4, s.length);
+            assertEquals("17", s[0]);
+            assertEquals("41196312", s[1]); 
+            assertEquals("41277500", s[2]);
         }
-
     }
-
-
+    
     
 
     /**
