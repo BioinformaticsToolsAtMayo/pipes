b0e5a68438a82635f090e459ec019afa338b4422
m089716 <m089716@8f329c14-d232-4f17-8684-7ee34322b8dc>
2012-12-20 19:30:29 +0000

    updated test cases

--- src/test/java/edu/mayo/pipes/JSON/tabix/SameVariantPipeTest.java
+++ src/test/java/edu/mayo/pipes/JSON/tabix/SameVariantPipeTest.java
@@ -4,6 +4,7 @@
  */
 package edu.mayo.pipes.JSON.tabix;
 
+import com.jayway.jsonpath.JsonPath;
 import com.tinkerpop.pipes.Pipe;
 import com.tinkerpop.pipes.util.Pipeline;
 import edu.mayo.pipes.PrintPipe;
@@ -20,7 +21,6 @@ import static org.junit.Assert.*;
 
 /**
  *
- * @author m102417
  */
 public class SameVariantPipeTest {
     
@@ -45,6 +45,20 @@ public class SameVariantPipeTest {
     
     private String catalogFile = "src/test/resources/testData/sameVariantCatalog.tsv.gz";
 
+    
+    /**
+     * POSSIBLE TEST CASES
+     * 1. Variant with no match.
+     * 2. Variant with 1 match.
+     * 3. Variant with more than 1 matches.
+     * 4. Variant with position that is same, but not matched
+     * 5. Variant with Chrom + Pos that is same, but not matched
+     * 6. Variant that matched on rsId + Chr + Pos, but if Flag is ON, then it shouldn't match
+     * 7. Variant that matched on ref/alt + Chr + Pos, but if Flag is ON, then it shouldn't match
+     * 8. If both Flags are ON.. match either way "OR" logic
+     * 
+     */
+    
     @Test
     public void testSomeMethod() {
 //        try {
@@ -64,6 +78,10 @@ public class SameVariantPipeTest {
     }
     
     
+    /**
+     * 1. Variant with no match.
+     * @throws IOException
+     */
     @Test
     public void testNoMatch() throws IOException{
         String variantThatDoesNotMatch = "20	24970000	rsFOOBAZ	T	C	.	.	.";
@@ -73,18 +91,51 @@ public class SameVariantPipeTest {
         History next = (History) p.next();
         assertEquals("{}", next.get(9));
     }
-    
+
+    /**
+     * 2. Variant with 1 match.
+     * @throws IOException
+     */
+    @Test
+    public void testOneMatch() throws IOException{
+        String variantOneMatch = "21	26960070	rs116645811	G	A	.	.	.";
+        Pipe same = new SameVariantPipe(catalogFile);
+        Pipeline p = new Pipeline(new HistoryInPipe(), new VCF2VariantPipe(), same);
+        p.setStarts(Arrays.asList(variantOneMatch));
+        
+        int resultCount = 0;
+        while(p.hasNext()) {
+        	History next = (History) p.next();
+        	resultCount++;
+            //System.out.println(next.get(9));
+            assertEquals("21", JsonPath.compile("CHROM").read(next.get(9)));
+            assertEquals("26960070", JsonPath.compile("POS").read(next.get(9)));
+            assertEquals("rs116645811", JsonPath.compile("ID").read(next.get(9)));
+        }
+        assertEquals(1, resultCount); //only one row need to be returned
+    }
+
+    /**
+     * 3. Variant with multiple matches.
+     * @throws IOException
+     */
     @Test
     public void testMultipleMatch() throws IOException{
-        String variantThatDoesNotMatch = "21	26976144	rs116331755	A	G	.	.	.";
+        String variantMultipleMatch = "21	26976144	rs116331755	A	G	.	.	.";
         Pipe same = new SameVariantPipe(catalogFile);
-        Pipeline p = new Pipeline(new HistoryInPipe(), new VCF2VariantPipe(), same, new PrintPipe());
-        p.setStarts(Arrays.asList(variantThatDoesNotMatch));
+        Pipeline p = new Pipeline(new HistoryInPipe(), new VCF2VariantPipe(), same);
+        p.setStarts(Arrays.asList(variantMultipleMatch));
+
+        int resultCount = 0;
         while(p.hasNext()){
             History next = (History) p.next();
-            
+        	resultCount++;
+        	
+        	// This loops goes thru 3 times, in each case, Chrom, Pos and ID must be the same. So this assert should not fail.
+            assertEquals("21", JsonPath.compile("CHROM").read(next.get(9)));
+            assertEquals("26976144", JsonPath.compile("POS").read(next.get(9)));
+            assertEquals("rs116331755", JsonPath.compile("ID").read(next.get(9)));
         }
-        
-        //assertEquals("{}", next.get(9));
+        assertEquals(3, resultCount); //only one row need to be returned
     }
 }
