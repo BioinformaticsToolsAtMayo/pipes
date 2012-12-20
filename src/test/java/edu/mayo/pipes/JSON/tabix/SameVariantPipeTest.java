061a1e55347db6f4af465b3218477669a098be97
m089716 <m089716@8f329c14-d232-4f17-8684-7ee34322b8dc>
2012-12-20 22:53:01 +0000

    added test cases

--- src/test/java/edu/mayo/pipes/JSON/tabix/SameVariantPipeTest.java
+++ src/test/java/edu/mayo/pipes/JSON/tabix/SameVariantPipeTest.java
@@ -51,32 +51,21 @@ public class SameVariantPipeTest {
      * 1. Variant with no match.
      * 2. Variant with 1 match.
      * 3. Variant with more than 1 matches.
-     * 4. Variant with position that is same, but not matched
+     * 4. Variant with Position that is same, but not matched
      * 5. Variant with Chrom + Pos that is same, but not matched
      * 6. Variant that matched on rsId + Chr + Pos, but if Flag is ON, then it shouldn't match
      * 7. Variant that matched on ref/alt + Chr + Pos, but if Flag is ON, then it shouldn't match
-     * 8. If both Flags are ON.. match either way "OR" logic
+     * 8. If both Flags are ON.. throw and exception
+     * 9. Test for ALT:
+     * 		in the file: C, T
+     * 		input: A
+     *      Output options: 
+     *      	A -> C : OK; 
+     *      	A -> T : OK; 
+     *      	A -> [C,T] : OK
+     *      	A -> any other than C, T.. FAIL
      * 
      */
-    
-    @Test
-    public void testSomeMethod() {
-//        try {
-//            System.out.println("Process Next Start...");
-//            Pipe p = new Pipeline(new CatPipe(), new HistoryInPipe(), new VCF2VariantPipe(), new SameVariantPipe(catalogFile));
-//            p.setStarts(Arrays.asList("src/test/resources/testData/sameVariant.vcf"));
-//            while(p.hasNext()){
-//                History next = (History) p.next();
-//                for(int i=0; i< next.size(); i++){
-//                    System.out.println(next.get(i));
-//                }
-//                System.out.println("****************************");
-//            }
-//        } catch (IOException ex) {
-//            Logger.getLogger(SameVariantPipeTest.class.getName()).log(Level.SEVERE, null, ex);
-//        }
-    }
-    
     
     /**
      * 1. Variant with no match.
@@ -115,7 +104,7 @@ public class SameVariantPipeTest {
         assertEquals(1, resultCount); //only one row need to be returned
     }
 
-    /**
+    /******************************************************************************************
      * 3. Variant with multiple matches.
      * @throws IOException
      */
@@ -138,4 +127,367 @@ public class SameVariantPipeTest {
         }
         assertEquals(3, resultCount); //only one row need to be returned
     }
+    
+    /***********************************************************************************
+     * 4. Variant with position that is same, but not matched
+     * 4a) Same Pos; Different Chr, Different rsId, same Ref, same Alt - return the match
+     * 4b) Same Pos; Different Chr, Different rsId, different Ref, same Alt - empty
+     * 4c) Same Pos; Different Chr, Different rsId, different Ref, different Alt - empty
+     */
+    @Test
+    public void testVariantSamePositionNoMatch1() throws IOException{
+        String variantSamePositionNoMatch = "22	26960070	BADRSID	G	A	.	.	.";
+        Pipe same = new SameVariantPipe(catalogFile);
+        Pipeline p = new Pipeline(new HistoryInPipe(), new VCF2VariantPipe(), same);
+        p.setStarts(Arrays.asList(variantSamePositionNoMatch));
+        History next = (History) p.next();
+        assertEquals("{}", next.get(9)); // EMPTY
+    }
+
+    /**
+     * 4b) Same Chr+Pos; Different rsId, different Ref, same Alt - empty
+     */
+    @Test
+    public void testVariantSamePositionNoMatch2() throws IOException{
+        String variantSamePositionNoMatch = "22	26960070	BADRSID	X	A	.	.	.";
+        Pipe same = new SameVariantPipe(catalogFile);
+        Pipeline p = new Pipeline(new HistoryInPipe(), new VCF2VariantPipe(), same);
+        p.setStarts(Arrays.asList(variantSamePositionNoMatch));
+        History next = (History) p.next();
+        assertEquals("{}", next.get(9)); // EMPTY
+    }
+
+    /**
+     * 4c) Same Chr+Pos; Different rsId, different Ref, different Alt - empty
+     */
+    @Test
+    public void testVariantSamePositionNoMatch3() throws IOException{
+        String variantSamePositionNoMatch = "22	26960070	BADRSID	X	Y	.	.	.";
+        Pipe same = new SameVariantPipe(catalogFile);
+        Pipeline p = new Pipeline(new HistoryInPipe(), new VCF2VariantPipe(), same);
+        p.setStarts(Arrays.asList(variantSamePositionNoMatch));
+        History next = (History) p.next();
+        assertEquals("{}", next.get(9)); // EMPTY
+    }
+
+    /******************************************************************************8
+     * 5. Variant with Chr+Position that is same, but not matched
+     * 5a) Same Chr+Pos; Different rsId, same Ref, same Alt - return the match
+     * 5b) Same Chr+Pos; Different rsId, different Ref, same Alt - empty
+     * 5c) Same Chr+Pos; Different rsId, different Ref, different Alt - empty
+     */
+    @Test
+    public void testVariantSameChrPositionNoMatch1() throws IOException{
+        String variantSamePositionNoMatch = "21	26960070	BADRSID	G	A	.	.	.";
+        Pipe same = new SameVariantPipe(catalogFile);
+        Pipeline p = new Pipeline(new HistoryInPipe(), new VCF2VariantPipe(), same);
+        p.setStarts(Arrays.asList(variantSamePositionNoMatch));
+
+        int resultCount = 0;
+        while(p.hasNext()){
+            History next = (History) p.next();
+        	resultCount++;
+        	
+        	// This loops goes thru 3 times, in each case, Chrom, Pos and ID must be the same. So this assert should not fail.
+            assertEquals("21", JsonPath.compile("CHROM").read(next.get(9)));
+            assertEquals("26960070", JsonPath.compile("POS").read(next.get(9)));
+            assertEquals("G", JsonPath.compile("REF").read(next.get(9)));
+            assertEquals("A", JsonPath.compile("ALT").read(next.get(9)));
+        }
+        assertEquals(1, resultCount); //only one row need to be returned
+    }
+
+    /**
+     * 5b) Same Chr+Pos; Different rsId, different Ref, same Alt - empty
+     */
+    @Test
+    public void testVariantSameChrPositionNoMatch2() throws IOException{
+        String variantSamePositionNoMatch = "21	26960070	BADRSID	X	A	.	.	.";
+        Pipe same = new SameVariantPipe(catalogFile);
+        Pipeline p = new Pipeline(new HistoryInPipe(), new VCF2VariantPipe(), same);
+        p.setStarts(Arrays.asList(variantSamePositionNoMatch));
+        History next = (History) p.next();
+        assertEquals("{}", next.get(9)); // EMPTY
+    }
+
+    /**
+     * 5c) Same Chr+Pos; Different rsId, different Ref, different Alt - empty
+     */
+    @Test
+    public void testVariantSameChrPositionNoMatch3() throws IOException{
+        String variantSamePositionNoMatch = "21	26960070	BADRSID	X	Y	.	.	.";
+        Pipe same = new SameVariantPipe(catalogFile);
+        Pipeline p = new Pipeline(new HistoryInPipe(), new VCF2VariantPipe(), same);
+        p.setStarts(Arrays.asList(variantSamePositionNoMatch));
+        History next = (History) p.next();
+        assertEquals("{}", next.get(9)); // EMPTY
+    }
+
+    /*************************************************************
+     * * 6. Variant that matched on rsId + Chr + Pos, but if rsIDFlag is ON, then it shouldn't match
+     */
+    @Test
+    public void testRsIdFlagTrue1() throws IOException{
+    	// RSID: Matched and rsIDFlag=True.. then return row or check based only on RSID
+    	String variantSamePositionNoMatch = "21\t26960070\trs116645811\tG\tA\t.\t.\t.";
+        Pipe same = new SameVariantPipe(catalogFile, true, false, -1);
+        Pipeline p = new Pipeline(new HistoryInPipe(), new VCF2VariantPipe(), same);
+        p.setStarts(Arrays.asList(variantSamePositionNoMatch));
+
+        History next = (History) p.next();
+       	assertEquals("rs116645811", JsonPath.compile("ID").read(next.get(9)));        	
+    }
+
+    /**
+     * * 6b. 
+     */
+    @Test
+    public void testRsIdFlagTrue2() throws IOException{
+    	// RSID: NOT MATCHED and rsIDFlag=FLASE.. then return EMPTY
+    	String variantSamePositionNoMatch = "21\t26960070\tBADRSID\tG\tA\t.\t.\t.";
+        Pipe same = new SameVariantPipe(catalogFile, true, false, -1);
+        Pipeline p = new Pipeline(new HistoryInPipe(), new VCF2VariantPipe(), same);
+        p.setStarts(Arrays.asList(variantSamePositionNoMatch));
+
+        History next = (History) p.next();
+       	assertEquals("{}", next.get(9));        	
+    }
+    
+    /**
+     * * 6c. 
+     */
+    @Test
+    public void testRsIdFlagFlase1() throws IOException{
+    	// RSID: MATCHED and rsIDFlag=FALSE.. then return ROWif found baed on chr, pos
+    	String variantSamePositionNoMatch = "21\t26960070\trs1166458118\tG\tA\t.\t.\t.";
+        Pipe same = new SameVariantPipe(catalogFile, false, false, -1);
+        Pipeline p = new Pipeline(new HistoryInPipe(), new VCF2VariantPipe(), same);
+        p.setStarts(Arrays.asList(variantSamePositionNoMatch));
+
+        History next = (History) p.next();
+        assertEquals("21", JsonPath.compile("CHROM").read(next.get(9)));
+        assertEquals("26960070", JsonPath.compile("POS").read(next.get(9)));
+       	assertEquals("rs116645811", JsonPath.compile("ID").read(next.get(9)));        	
+    }
+    
+
+    /**
+     * * 6d. 
+     */
+    @Test
+    public void testRsIdFlagFlase2() throws IOException{
+    	// RSID: NOT MATCHED and rsIDFlag=FALSE.. then return ROWif found baed on chr, pos
+    	String variantSamePositionNoMatch = "21\t26960070\tBADRSID\tG\tA\t.\t.\t.";
+        Pipe same = new SameVariantPipe(catalogFile, false, false, -1);
+        Pipeline p = new Pipeline(new HistoryInPipe(), new VCF2VariantPipe(), same);
+        p.setStarts(Arrays.asList(variantSamePositionNoMatch));
+
+        History next = (History) p.next();
+        assertEquals("21", JsonPath.compile("CHROM").read(next.get(9)));
+        assertEquals("26960070", JsonPath.compile("POS").read(next.get(9)));
+        assertEquals("rs116645811", JsonPath.compile("ID").read(next.get(9)));       	        	
+    }
+    
+    /****************************************************
+     * 7. Variant that matched on ref/alt + Chr + Pos, but if AlleleFlag is ON, then it shouldn't match
+     * 
+     * 7a: Ref: Matched, Alt: is a subset and AlleleFlag=True.. then return row or check based only on RSID
+     */
+    @Test 
+    public void testAlleleFlagTrue1() throws IOException{
+    	// Ref: Matched, Alt: is a subset and AlleleFlag=True.. then return row or check based only on RSID
+    	String variantSamePositionNoMatch = "21\t26960070\trs116645811\tG\tA\t.\t.\t.";
+        Pipe same = new SameVariantPipe(catalogFile, false, true, -1);
+        Pipeline p = new Pipeline(new HistoryInPipe(), new VCF2VariantPipe(), same);
+        p.setStarts(Arrays.asList(variantSamePositionNoMatch));
+
+        History next = (History) p.next();
+       	assertEquals("rs116645811", JsonPath.compile("ID").read(next.get(9)));        	
+    }
+    
+   /** 7b: Ref: Not Matched, Alt: is a subset and AlleleFlag=True.. then return EMPTY 
+     * 
+    */
+   @Test 
+   public void testAlleleFlagTrue2() throws IOException{
+   	// Ref: Not Matched, Alt: is a subset and AlleleFlag=True.. then return EMPTY
+   	String variantSamePositionNoMatch = "21\t26960070\trs116645811\tX\tA\t.\t.\t.";
+       Pipe same = new SameVariantPipe(catalogFile, false, true, -1);
+       Pipeline p = new Pipeline(new HistoryInPipe(), new VCF2VariantPipe(), same);
+       p.setStarts(Arrays.asList(variantSamePositionNoMatch));
+
+       History next = (History) p.next();
+       assertEquals("{}", next.get(9));        	
+   }
+   
+  /** 7c: Ref: Matched, Alt: is NOT a subset and AlleleFlag=True.. then return EMPTY 
+  */
+  @Test 
+  public void testAlleleFlagTrue3() throws IOException{
+  	// Ref: Matched, Alt: is NOT a subset and AlleleFlag=True.. then return EMPTY
+  	String variantSamePositionNoMatch = "21	34022588	rs115683257	C	G,T	.	.	.";
+      Pipe same = new SameVariantPipe(catalogFile, false, true, -1);
+      Pipeline p = new Pipeline(new HistoryInPipe(), new VCF2VariantPipe(), same);
+      p.setStarts(Arrays.asList(variantSamePositionNoMatch));
+
+      History next = (History) p.next();
+      assertEquals("{}", next.get(9));        	
+  }
+   
+  /** 7d: 
+   * FLASE, FALSE: Match by rsID or Allele 
+  */
+  @Test 
+  public void testAlleleFlagFalse1() throws IOException{
+	  // RSID: MATCH - Ref: Matched, Alt: NOT a subset and AlleleFlag=True.. then return Row, since RSID matches
+  	  String variantSamePositionNoMatch = "21	34022588	rs115683257	C	G	.	.	.";
+  	  Pipe same = new SameVariantPipe(catalogFile, false, false, -1);
+      Pipeline p = new Pipeline(new HistoryInPipe(), new VCF2VariantPipe(), same);
+      p.setStarts(Arrays.asList(variantSamePositionNoMatch));
+
+      History next = (History) p.next();
+      assertEquals("rs115683257", JsonPath.compile("ID").read(next.get(9)));
+  }
+
+  /** 7e: 
+   * FLASE, FALSE: Match by rsID or Allele 
+  */
+  @Test 
+  public void testAlleleFlagFalse2() throws IOException{
+	  // RSID: MATCH - Ref: Not Matched, Alt: NOT a subset and AlleleFlag=True.. then return Row, since RSID matches
+  	  String variantSamePositionNoMatch = "21	34022588	rs115683257	T	G	.	.	.";
+  	  Pipe same = new SameVariantPipe(catalogFile, false, false, -1);
+      Pipeline p = new Pipeline(new HistoryInPipe(), new VCF2VariantPipe(), same);
+      p.setStarts(Arrays.asList(variantSamePositionNoMatch));
+
+      History next = (History) p.next();
+      assertEquals("rs115683257", JsonPath.compile("ID").read(next.get(9)));
+  }
+
+
+  /** 7f: 
+   * FLASE, FALSE: Match by rsID or Allele 
+  */
+  @Test 
+  public void testAlleleFlagFalse3() throws IOException{
+	  // RSID: NO MATCH - Ref: Matched, Alt: Is a subset and AlleleFlag=True.. then return Row, since REF matches
+  	  String variantSamePositionNoMatch = "21	34022588	BADRSID	C	A,C	.	.	.";
+  	  Pipe same = new SameVariantPipe(catalogFile, false, false, -1);
+      Pipeline p = new Pipeline(new HistoryInPipe(), new VCF2VariantPipe(), same);
+      p.setStarts(Arrays.asList(variantSamePositionNoMatch));
+
+      History next = (History) p.next();
+      assertEquals("rs115683257", JsonPath.compile("ID").read(next.get(9)));
+  }
+
+  /** 7g: 
+   * FLASE, FALSE: Match by rsID or Allele 
+  */
+  @Test 
+  public void testAlleleFlagFalse4() throws IOException{
+	  // RSID: NO MATCH - Ref: Matched, Alt: NOT a subset and AlleleFlag=True.. then return EMPTY
+  	  String variantSamePositionNoMatch = "21	34022588	BADRSID	C	G	.	.	.";
+  	  Pipe same = new SameVariantPipe(catalogFile, false, false, -1);
+      Pipeline p = new Pipeline(new HistoryInPipe(), new VCF2VariantPipe(), same);
+      p.setStarts(Arrays.asList(variantSamePositionNoMatch));
+
+      History next = (History) p.next();
+      assertEquals("{}", next.get(9));
+  }
+
+  
+  /****************************************************
+     * 8. If both flags are set to TRUE, then throw exception
+     */
+    @Test (expected=IllegalArgumentException.class)
+    public void testFlagsSetToTrue() throws IOException{
+    	// RSID: NOT MATCHED and rsIDFlag=FALSE.. then return ROWif found baed on chr, pos
+    	String variantSamePositionNoMatch = "DOESNT MATTER";
+        
+    	Pipe same = new SameVariantPipe(catalogFile, true, true, -1);
+    }
+
+    
+    /********************************************************************************88
+     * 9. Test for ALT:
+     * 		in the file: C, T
+     * 		input: A
+     *      Output options: 
+     *      	A -> C : OK; 
+     *      	A -> T : OK; 
+     *      	A -> [C,T] : OK
+     *      	A -> any other than C, T.. FAIL
+     */
+    @Test
+    public void testAltAlleleSubset1() throws IOException {
+    	//ALT: in file "A,C" .. giving Subset: "A"
+    	String variantSamePositionNoMatch = "21	34022588	rs115683257	C	A	.	.	.";
+        Pipe same = new SameVariantPipe(catalogFile);
+        Pipeline p = new Pipeline(new HistoryInPipe(), new VCF2VariantPipe(), same);
+        p.setStarts(Arrays.asList(variantSamePositionNoMatch));
+
+        History next = (History) p.next();
+    	assertEquals("A,C", JsonPath.compile("ALT").read(next.get(9)));
+    }
+
+    /**
+     * 9a. 
+	*/
+    @Test
+    public void testAltAlleleSubset2() throws IOException {
+        //ALT: in file "A,C" .. giving Subset: "C"
+    	String variantSamePositionNoMatch = "21	34022588	rs115683257	C	C	.	.	.";
+        Pipe same = new SameVariantPipe(catalogFile);
+        Pipeline p = new Pipeline(new HistoryInPipe(), new VCF2VariantPipe(), same);
+        p.setStarts(Arrays.asList(variantSamePositionNoMatch));
+        History next = (History) p.next();
+    	assertEquals("A,C", JsonPath.compile("ALT").read(next.get(9)));
+    }
+
+    /**
+     * 9b. 
+	*/
+    @Test
+    public void testAltAlleleFullList1() throws IOException {
+        //ALT: in file "T,C" .. Ref: C ; Alt: "T,C"
+    	String variantSamePositionNoMatch = "21\t34058146\trs114942253\tC\tT,C\t.\t.\t.";
+        Pipe same = new SameVariantPipe(catalogFile, false, true, -1);
+        Pipeline p = new Pipeline(new HistoryInPipe(), new VCF2VariantPipe(), same);
+        p.setStarts(Arrays.asList(variantSamePositionNoMatch));
+        History next = (History) p.next();
+        assertEquals("T,C", JsonPath.compile("ALT").read(next.get(9)));        	
+    }
+
+    /**
+     * 9c. 
+	*/
+    @Test
+    public void testAltAlleleFullList2() throws IOException {
+        //ALT: in file "T,C" .. Ref: T ; Alt: "T,C"
+    	String variantSamePositionNoMatch = "21\t34058146\trs114942253\tT\tT,C\t.\t.\t.";
+        Pipe same = new SameVariantPipe(catalogFile, false, true, -1);
+        Pipeline p = new Pipeline(new HistoryInPipe(), new VCF2VariantPipe(), same);
+        p.setStarts(Arrays.asList(variantSamePositionNoMatch));
+        History next = (History) p.next();
+        assertEquals("{}", next.get(9));        	
+    }
+    
+    /**
+     * 9d. 
+	*/
+    @Test
+    public void testAltAlleleFullList3() throws IOException {
+        //ALT: in file "T,C" .. Ref: C ; Alt: "T" -- 2 matches
+    	String variantSamePositionNoMatch = "21\t34058146\trs114942253\tC\tT\t.\t.\t.";
+        Pipe same = new SameVariantPipe(catalogFile, false, true, -1);
+        Pipeline p = new Pipeline(new HistoryInPipe(), new VCF2VariantPipe(), same);
+        p.setStarts(Arrays.asList(variantSamePositionNoMatch));
+        int resultCount=0;
+        while(p.hasNext()) {
+        	History next = (History) p.next();        	
+        	resultCount++;
+        }
+        assertEquals(2, resultCount);        	
+    }    
+
 }
