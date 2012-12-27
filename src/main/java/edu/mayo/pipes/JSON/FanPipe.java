1f9fa4bf82137323a838bbdbfa7c07feeecdd79a
m102417 <m102417@8f329c14-d232-4f17-8684-7ee34322b8dc>
2012-12-27 21:48:33 +0000

    new pipe to fan out a JSON array

---
+++ src/main/java/edu/mayo/pipes/JSON/FanPipe.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.mayo.pipes.JSON;
+
+/**
+ *
+ * @author m102417
+ */
+public class FanPipe {
+    //First things first, whatever, we are looking at needs to be "fan-able"
+    //A column can be fan-able if it is a JSON array, or if it is of type JSON and the
+    //path has multiple values.
+    
+}
