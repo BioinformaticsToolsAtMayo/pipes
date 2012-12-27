595331d61ad4176797eafba67bb032aa6e3d735c
m102417 <m102417@8f329c14-d232-4f17-8684-7ee34322b8dc>
2012-12-27 21:48:22 +0000

    new pipe to fan out a JSON array

---
+++ src/test/java/edu/mayo/pipes/JSON/FanPipeTest.java
@@ -0,0 +1,40 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.mayo.pipes.JSON;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author m102417
+ */
+public class FanPipeTest {
+    
+    public FanPipeTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+        String s = "[\"A|ENSG00000260583|ENST00000567517|Transcript|upstream_gene_variant|||||||4432|||\",\"A|ENSG00000154719|ENST00000352957|Transcript|intron_variant||||||||||\",\"A|ENSG00000154719|ENST00000307301|Transcript|missense_variant|1043|1001|334|T\\/M|aCg\\/aTg|||tolerated(0.05)|benign(0.001)|\"]]";
+                
+    }
+}
