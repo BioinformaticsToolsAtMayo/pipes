1c8343ee06d5d7b259ac5f9ee90734f99cc3e9fd
m102417 <m102417@8f329c14-d232-4f17-8684-7ee34322b8dc>
2012-12-28 15:56:16 +0000

    working version of the FanPipe

--- src/test/java/edu/mayo/pipes/JSON/FanPipeTest.java
+++ src/test/java/edu/mayo/pipes/JSON/FanPipeTest.java
@@ -4,6 +4,13 @@
  */
 package edu.mayo.pipes.JSON;
 
+import com.tinkerpop.pipes.Pipe;
+import com.tinkerpop.pipes.util.Pipeline;
+import edu.mayo.pipes.PrintPipe;
+import edu.mayo.pipes.SplitPipe;
+import edu.mayo.pipes.history.HistoryInPipe;
+import edu.mayo.pipes.history.HistoryOutPipe;
+import java.util.Arrays;
 import org.junit.*;
 import static org.junit.Assert.*;
 
@@ -33,8 +40,23 @@ public class FanPipeTest {
     }
 
     @Test
-    public void testSomeMethod() {
-        String s = "[\"A|ENSG00000260583|ENST00000567517|Transcript|upstream_gene_variant|||||||4432|||\",\"A|ENSG00000154719|ENST00000352957|Transcript|intron_variant||||||||||\",\"A|ENSG00000154719|ENST00000307301|Transcript|missense_variant|1043|1001|334|T\\/M|aCg\\/aTg|||tolerated(0.05)|benign(0.001)|\"]]";
-                
+    public void test() {
+        //String s = "[\"A|ENSG00000260583|ENST00000567517|Transcript|upstream_gene_variant|||||||4432|||\",\"A|ENSG00000154719|ENST00000352957|Transcript|intron_variant||||||||||\",\"A|ENSG00000154719|ENST00000307301|Transcript|missense_variant|1043|1001|334|T\\/M|aCg\\/aTg|||tolerated(0.05)|benign(0.001)|\"]]";
+        System.out.println("Testing Fan Pipe Basic Functionality...");        
+        String input = "X\tY\tZ\t[\"A\",\"B\",\"C\"]";
+        Pipe p = new Pipeline(new HistoryInPipe(), new FanPipe(), new HistoryOutPipe() );
+        p.setStarts(Arrays.asList(input));
+        for(int i=0;p.hasNext();i++){
+            String s = (String) p.next();
+            if(i==1){
+                assertEquals("X\tY\tZ\tA",s);
+            }
+            if(i==2){
+                assertEquals("X\tY\tZ\tB",s);
+            }
+            if(i==3){
+                assertEquals("X\tY\tZ\tC",s);
+            }
+        }
     }
 }
