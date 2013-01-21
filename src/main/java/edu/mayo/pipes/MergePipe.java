e634dcee79acb43d3411108da6109259246a7086
m054457 <m054457@8f329c14-d232-4f17-8684-7ee34322b8dc>
2013-01-21 16:14:22 +0000

    Newline was not getting added when there was only one column

--- src/main/java/edu/mayo/pipes/MergePipe.java
+++ src/main/java/edu/mayo/pipes/MergePipe.java
@@ -14,38 +14,36 @@ import java.util.NoSuchElementException;
  */
 public class MergePipe extends AbstractPipe<List<String>, String>{
     private String delimiter = "\t";
-    private boolean newline = false;
+    private boolean mIsAddNewline = false;
     public MergePipe(String delim){
         this.delimiter = delim;
     }
 
     public MergePipe(String delim, boolean appendNewlines){
         this.delimiter = delim;
-        newline = appendNewlines;
+        mIsAddNewline = appendNewlines;
     }
     
     @Override
     protected String processNextStart() throws NoSuchElementException {
-        if(this.starts.hasNext()){
-            StringBuilder sb = new StringBuilder();
-            List<String> l = this.starts.next();
-            if(l.size()==1){
-                return l.get(0);
-            }
-            for(int i=0;i<l.size()-1;i++){
-                String s = l.get(i);
-                sb.append(s);
-                sb.append(delimiter);
-            }
-            sb.append(l.get(l.size()-1));
-            if(newline){
-                sb.append("\n");
-            }
-            return sb.toString();
-        }else {
-            throw new NoSuchElementException();
-        }
+        if( ! this.starts.hasNext() )
+        	throw new NoSuchElementException();
         
+	    StringBuilder sb = new StringBuilder();
+	    List<String> cols = this.starts.next();
+	    if(cols.size()==1){
+	    	return cols.get(0) + (mIsAddNewline ? "\n" : "");
+	    }
+	    for(int i=0;i<cols.size()-1;i++){
+	    	String s = cols.get(i);
+	    	sb.append(s);
+	    	sb.append(delimiter);
+	    }
+	    sb.append(cols.get(cols.size()-1));
+	    if(mIsAddNewline){
+	    	sb.append("\n");
+	    }
+	    return sb.toString();
     }
     
 }
