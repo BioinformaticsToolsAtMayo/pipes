c6bb618aa4e08b17c67ad47026c6432aff893477
m054457 <m054457@8f329c14-d232-4f17-8684-7ee34322b8dc>
2013-01-23 16:14:37 +0000

    Condensed EOL checks and removed single-column check since it is in the for loop

--- src/main/java/edu/mayo/pipes/MergePipe.java
+++ src/main/java/edu/mayo/pipes/MergePipe.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.mayo.pipes;
 
 import com.tinkerpop.pipes.AbstractPipe;
@@ -10,10 +6,11 @@ import java.util.NoSuchElementException;
 
 /**
  *  MergePipe takes a list of strings, and merges them into a single string.
- * @author m102417
+ * @author m102417 Daniel Quest, Michael Meiners
  */
 public class MergePipe extends AbstractPipe<List<String>, String>{
     private String delimiter = "\t";
+    private final String EOL = "\n";
     private boolean mIsAddNewline = false;
     public MergePipe(String delim){
         this.delimiter = delim;
@@ -31,17 +28,12 @@ public class MergePipe extends AbstractPipe<List<String>, String>{
         
 	    StringBuilder sb = new StringBuilder();
 	    List<String> cols = this.starts.next();
-	    if(cols.size()==1){
-	    	return cols.get(0) + (mIsAddNewline ? "\n" : "");
-	    }
-	    for(int i=0;i<cols.size()-1;i++){
-	    	String s = cols.get(i);
-	    	sb.append(s);
-	    	sb.append(delimiter);
-	    }
-	    sb.append(cols.get(cols.size()-1));
-	    if(mIsAddNewline){
-	    	sb.append("\n");
+	    for(int i=0;i<cols.size();i++){
+	    	sb.append(cols.get(i));
+	    	if(i < cols.size()-1)
+	    		sb.append(delimiter);
+	    	else if(mIsAddNewline)
+	    		sb.append(EOL);
 	    }
 	    return sb.toString();
     }
