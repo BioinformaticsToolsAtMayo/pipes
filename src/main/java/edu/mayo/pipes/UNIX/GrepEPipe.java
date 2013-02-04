39972b7279325f504f52bb7ce83e8d15059f4b18
m054457 <m054457@8f329c14-d232-4f17-8684-7ee34322b8dc>
2013-02-04 19:55:35 +0000

    Both GrepEPipe and GrepPipe now use a reluctant reg expression match so it doesn't have to match the entire line - it will return a result if ANY part of the line matches the regular expression

--- src/main/java/edu/mayo/pipes/UNIX/GrepEPipe.java
+++ src/main/java/edu/mayo/pipes/UNIX/GrepEPipe.java
@@ -4,14 +4,17 @@
  */
 package edu.mayo.pipes.UNIX;
 
-import com.tinkerpop.pipes.AbstractPipe;
-import com.tinkerpop.pipes.PipeFunction;
-import com.tinkerpop.pipes.filter.FilterFunctionPipe;
-import com.tinkerpop.pipes.filter.FilterPipe;
 import java.util.NoSuchElementException;
 
+import com.tinkerpop.pipes.AbstractPipe;
+import com.tinkerpop.pipes.filter.FilterPipe;
+
 /**
- * This pipe is the negative of GrepPipe that works just like the unix grep utility
+ * This pipe is the negative of GrepPipe (that works just like the unix grep utility), 
+ * in that it will only return results that do NOT match the given regular expression
+ * See:
+ *   http://stackoverflow.com/questions/10198713/how-to-find-out-if-a-substring-of-a-java-string-matches-a-regular-expression
+ *   http://javamex.com/tutorials/regular_expressions/repetition_greedy_reluctant.shtml#.UQgvNegzKts
  * @author dquest
  */
 public class GrepEPipe extends AbstractPipe<String, String> implements FilterPipe<String> {
@@ -22,7 +25,9 @@ public class GrepEPipe extends AbstractPipe<String, String> implements FilterPip
     protected String processNextStart() {
         while (this.starts.hasNext()) {
             String s = this.starts.next();
-            if (!s.matches(regex)) {
+            // Use a reluctant operator on each side of the supplied regex 
+            // expression so it doesn't have to match the entire string
+           if (!s.matches(".*?" + regex + ".*?")) {
                 return s;
             }
         }
--- src/main/java/edu/mayo/pipes/UNIX/GrepPipe.java
+++ src/main/java/edu/mayo/pipes/UNIX/GrepPipe.java
@@ -12,6 +12,9 @@ import java.util.NoSuchElementException;
 
 /**
  * This pipe works just like the unix grep utility
+ * See:
+ *   http://stackoverflow.com/questions/10198713/how-to-find-out-if-a-substring-of-a-java-string-matches-a-regular-expression
+ *   http://javamex.com/tutorials/regular_expressions/repetition_greedy_reluctant.shtml#.UQgvNegzKts
  * @author dquest
  */
 public class GrepPipe extends AbstractPipe<String, String> implements FilterPipe<String> {
@@ -22,7 +25,9 @@ public class GrepPipe extends AbstractPipe<String, String> implements FilterPipe
     protected String processNextStart() {
         while (this.starts.hasNext()) {
             String s = this.starts.next();
-            if (s.matches(regex)) {
+            // Use a reluctant operator on each side of the supplied regex 
+            // expression so it doesn't have to match the entire string
+            if(s.matches(".*?" + regex + ".*?")) {
                 return s;
             }
         }
