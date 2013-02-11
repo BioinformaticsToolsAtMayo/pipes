76dc20c011286c174ce4eab09a30a472f51590c5
m102417 <m102417@8f329c14-d232-4f17-8684-7ee34322b8dc>
2013-02-11 20:28:04 +0000

    making the new lookup pipe

---
+++ src/main/java/edu/mayo/pipes/JSON/lookup/LookupPipe.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.mayo.pipes.JSON.lookup;
+
+/**
+ *
+ * @author m102417
+ */
+public class LookupPipe {
+    
+    public LookupPipe(String h2db){
+        
+    }
+    
+}
