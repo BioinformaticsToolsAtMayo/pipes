605f47f669b18500ae92253874449a8006156c55
mrm4408 <mrm4408@8f329c14-d232-4f17-8684-7ee34322b8dc>
2013-05-31 01:51:37 +0000

    BugzID: 2333 Fixed issue where flat file has a header but no data.

--- src/main/java/edu/mayo/pipes/history/HistoryInPipe.java
+++ src/main/java/edu/mayo/pipes/history/HistoryInPipe.java
@@ -38,65 +38,103 @@ public class HistoryInPipe extends AbstractPipe<String, History> {
 	private final String COL_DELIMITER = "\t";
 	
 	@Override
-	protected History processNextStart() throws NoSuchElementException {
+	protected History processNextStart() throws NoSuchElementException
+	{
 
 		String line = this.starts.next();
 
 		History history = new History();
 
-		if (history.isMetaDataInitialized() == false) {
-			// fast forward and capture header rows until we hit the first data
-			// row
+		if (history.isMetaDataInitialized() == false)
+		{
+
+
+			// fast forward and capture header rows until we hit the first data row
 			List<String> headerRows = new ArrayList<String>();
-			while (line.startsWith("#")) {
+			while (line.startsWith("#"))
+			{
 				headerRows.add(line);
 
-				line = this.starts.next();
-			}
-			HistoryMetaData hMeta = new HistoryMetaData(headerRows);
-
-			// process column header if present
-			if (headerRows.size() > 0) {
-				String colHeaderLine = headerRows.get(headerRows.size() - 1);
-
-				// trim off leading #
-				colHeaderLine = colHeaderLine.substring(1);
-
-				for (String colName : colHeaderLine.split(COL_DELIMITER)) {
-					ColumnMetaData cmd = new ColumnMetaData(colName);
-					hMeta.getColumns().add(cmd);
+				try
+				{
+					line = this.starts.next();
 				}
-			} else {
-				int numCols = line.split(COL_DELIMITER).length;
-				if(numCols != this.expand2NumCols && this.expand2NumCols != -1){
-					while(history.size() < expand2NumCols){
-						history.add("");
-					}
-					numCols= this.expand2NumCols;
-				}
-				// if there is no column header, just mark each column as
-				// UNKNOWN
-				for (int i = 1; i <= numCols; i++) {
-                                        ColumnMetaData cmd;
-                                        if(i==1){
-                                            cmd = new ColumnMetaData("UNKNOWN_" + i);
-                                        }else {
-                                            cmd = new ColumnMetaData("#UNKNOWN_" + i);
-                                        }
-					hMeta.getColumns().add(cmd);
+				catch (NoSuchElementException e)
+				{
+					// attempted to grab next line, but there are no more
+					// means there are no data lines after header lines
+
+					// # of columns based on last header row
+					int numCols = line.split(COL_DELIMITER).length;
+					
+					initializeMetaData(history, headerRows, numCols);
+					
+					// re-throw to tell next pipe there are no more History objects
+					throw e;
 				}
 			}
-
-			history.setMetaData(hMeta);
+			
+			// # of columns based on 1st data line
+			int numCols = line.split(COL_DELIMITER).length;
+			
+			initializeMetaData(history, headerRows, numCols);
 		}
 
 		// split data row, add to history
 		// SAFE split is required because there may be empty fields between delimiters
 		String[] colDataArr = StringUtils.safeSplit(line, COL_DELIMITER);
-		for (String colData : colDataArr) {
+		for (String colData : colDataArr)
+		{
 			history.add(colData);
 		}
 
 		return history;
+	}
+	
+	/**
+	 * Initializes the History metadata.
+	 * 
+	 * @param history
+	 * @param headerRows
+	 * @param numCols
+	 */
+	private void initializeMetaData(History history, List<String> headerRows, int numCols)
+	{
+		HistoryMetaData hMeta = new HistoryMetaData(headerRows);
+
+		// process column header if present
+		if (headerRows.size() > 0)
+		{
+			String colHeaderLine = headerRows.get(headerRows.size() - 1);
+
+			// trim off leading #
+			colHeaderLine = colHeaderLine.substring(1);
+
+			for (String colName : colHeaderLine.split(COL_DELIMITER))
+			{
+				ColumnMetaData cmd = new ColumnMetaData(colName);
+				hMeta.getColumns().add(cmd);
+			}
+		} 
+		else 
+		{
+			if(numCols != this.expand2NumCols && this.expand2NumCols != -1)
+			{
+				while(history.size() < expand2NumCols)
+				{
+					history.add("");
+				}
+				numCols= this.expand2NumCols;
+			}
+			
+			// if there is no column header, just mark each column as UNKNOWN
+			for (int i = 1; i <= numCols; i++)
+			{
+				ColumnMetaData cmd = new ColumnMetaData("#UNKNOWN_" + i);
+				hMeta.getColumns().add(cmd);
+			}
+		}
+
+		history.setMetaData(hMeta);		
 	}
 }
