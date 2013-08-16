1cb4c654c7f89894bb840379b3d90694ce7a45d0
m054457 <m054457@8f329c14-d232-4f17-8684-7ee34322b8dc>
2013-08-16 18:57:40 +0000

    Added method to save ColumnMetadata to a file with header, and to merge two lists of ColumnMetaData objects

--- src/main/java/edu/mayo/pipes/history/ColumnMetaData.java
+++ src/main/java/edu/mayo/pipes/history/ColumnMetaData.java
@@ -1,10 +1,16 @@
 package edu.mayo.pipes.history;
 
-
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+
+import org.apache.commons.io.FileExistsException;
 
 /**
  * Describes ColumnName, Type, Count, and Description 
@@ -34,11 +40,14 @@ public class ColumnMetaData {
 	public ColumnMetaData(String colName) {
 		columnName = colName;
 	}
+	
+	public ColumnMetaData() { }
+	
 
     public ColumnMetaData(String colName, String type, String count, String desc){
         this(colName, Type.valueOf(type),count,desc);
     }
-	
+
 	public ColumnMetaData(String colName, Type type, String count, String desc) {
 		this.columnName = colName;
 		this.type = type;
@@ -49,8 +58,71 @@ public class ColumnMetaData {
 	public String getColumnName() {
 		return columnName;
 	}
+	
+	/** Save a list of ColumnMetaData objects to a file, along with the header 
+	 * @throws IOException */
+	public static void saveMetaData(String columnsTsvPath, List<ColumnMetaData> colMetaList) throws IOException {
+		// Add descriptive info at top:
+		StringBuilder desc = new StringBuilder();
+		desc.append("##-----------------------------------------------------\n");
+		desc.append("## Catalog field definitions\n");
+		desc.append("##-----------------------------------------------------\n");
+		desc.append("## ColumnName=The key or column name\n");
+		desc.append("## Type=The type of the object, as can be determined from parsing the VCF file or taking and educated guess based on the catalog values (Possible values: JSON, JSONArray, String, Integer, Float, Boolean)\n"); 
+		desc.append("## Count=The number of values that repeatedly occur  (Possible values: 0 (Boolean), 1,2,..,n  or '.' for variable or unknown number of values\n");
+		desc.append("## Description=The description of the ColumnName\n");
+		desc.append("##-----------------------------------------------------\n");
+		desc.append("#ColumnName	Type	Count	Description\n");
 
-    public HashMap<String,ColumnMetaData> parseColumnProperties(String columnPropertiesPath) throws IOException {
+		// If the file already exists, throw exception
+		File columnsTsvFile = new File(columnsTsvPath);
+		if( columnsTsvFile.exists() )
+			throw new FileExistsException(columnsTsvFile);
+
+		// Write output to file
+		FileOutputStream fout = new FileOutputStream(new File(columnsTsvPath));
+
+		// Write header
+		fout.write( (desc.toString() + "\n").getBytes());
+		
+		// Loop thru all lines and write them
+		for(ColumnMetaData colMeta : colMetaList) {
+			fout.write( (colMeta.toString() + "\n").getBytes() );
+		}
+		
+		fout.close();
+	}
+	
+	/** Return a tab-delimited string in the format:  ColumnName \t Type \t Count \t Description */
+	public String toString() {
+		return this.columnName + "\t" + this.type.toString() + "\t" + this.count + "\t" + this.description;
+	}
+	
+	/** Merge two lists of ColumnMetaData objects, and return the combined list.
+	 *  NOTE: Items in the left list will take precedence over items in the right list (that have the same name) */
+	public static List<ColumnMetaData> merge(List<ColumnMetaData> colMetaListMain, List<ColumnMetaData> colMetaListAdditional) {
+		HashSet<String> colNamesAlreadyAdded = new HashSet<String>();
+		List<ColumnMetaData> mergedList = new ArrayList<ColumnMetaData>();
+		
+		// Add all items from the first list
+		for(ColumnMetaData colMeta : colMetaListMain) {
+			mergedList.add(colMeta);
+			colNamesAlreadyAdded.add(colMeta.columnName);
+		}
+		
+		// Now add all items from the right list if they have NOT yet been added.
+		for(ColumnMetaData colMeta : colMetaListAdditional) {
+			if( ! colNamesAlreadyAdded.contains(colMeta.columnName) ) {
+				mergedList.add(colMeta);
+				colNamesAlreadyAdded.add(colMeta.columnName);
+			}
+		}
+
+		return mergedList;
+	}
+	
+
+    public static HashMap<String,ColumnMetaData> parseColumnProperties(String columnPropertiesPath) throws IOException {
         HashMap<String,ColumnMetaData> descriptions = new HashMap<String,ColumnMetaData>();
         BufferedReader bReader = new BufferedReader(new FileReader(columnPropertiesPath));
         String line;
