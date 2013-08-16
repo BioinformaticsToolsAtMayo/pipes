71182c31cefd380be02a835d70bf9f3d9e84a28e
m102417 <m102417@8f329c14-d232-4f17-8684-7ee34322b8dc>
2013-08-16 13:50:26 +0000

    updated to have loading code

--- src/main/java/edu/mayo/pipes/history/ColumnMetaData.java
+++ src/main/java/edu/mayo/pipes/history/ColumnMetaData.java
@@ -1,6 +1,11 @@
 package edu.mayo.pipes.history;
 
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+
 /**
  * Describes ColumnName, Type, Count, and Description 
  * that will be used in the ##BIOR metadata header lines,
@@ -29,6 +34,10 @@ public class ColumnMetaData {
 	public ColumnMetaData(String colName) {
 		columnName = colName;
 	}
+
+    public ColumnMetaData(String colName, String type, String count, String desc){
+        this(colName, Type.valueOf(type),count,desc);
+    }
 	
 	public ColumnMetaData(String colName, Type type, String count, String desc) {
 		this.columnName = colName;
@@ -40,4 +49,26 @@ public class ColumnMetaData {
 	public String getColumnName() {
 		return columnName;
 	}
+
+    public HashMap<String,ColumnMetaData> parseColumnProperties(String columnPropertiesPath) throws IOException {
+        HashMap<String,ColumnMetaData> descriptions = new HashMap<String,ColumnMetaData>();
+        BufferedReader bReader = new BufferedReader(new FileReader(columnPropertiesPath));
+        String line;
+        while ((line = bReader.readLine()) != null) {
+            if(line.startsWith("#")){//it is a comment, chuck it
+                ;
+            }else {  //contains data
+                String[] split = line.split("\t");
+                if(split.length > 3){
+                    String desc = "";
+                    for(int i=3; split.length > i; i++){ //if the description had tabs -- remove them
+                        desc = desc + split[i];
+                    }
+                    ColumnMetaData cd = new ColumnMetaData(split[0], split[1], split[2], desc);
+                    descriptions.put(split[0],cd); //key is the fieldName
+                }
+            }
+        }
+        return descriptions;
+    }
 }
